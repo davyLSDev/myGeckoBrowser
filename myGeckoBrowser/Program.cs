2a8a7b967b555dbf2d09b105340f9d3161f0b8e8
Davy Tenn <davy.lsdev@gmail.com>
2014-12-16 23:15:16 -0700

    Make a very basic browser

---
+++ myGeckoBrowser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Gecko;
+
+namespace myGeckoBrowser
+{
+	static class myGeckoBrowser
+	{
+		[STAThread]
+		static void Main()
+		{
+			string xulRunnerLocation = XULRunnerLocator.GetXULRunnerLocation();
+			if (String.IsNullOrEmpty(xulRunnerLocation))
+				throw new ApplicationException("The XULRunner library is missing or has the wrong version");
+			string librarySearchPath = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH") ?? String.Empty;
+			if (!librarySearchPath.Contains(xulRunnerLocation))
+				throw new ApplicationException("LD_LIBRARY_PATH must contain " + xulRunnerLocation); 
+
+			Xpcom.Initialize (xulRunnerLocation);
+
+			var form = new Form();
+			var myBrowser = new GeckoWebBrowser {Dock = DockStyle.Fill};
+			myBrowser.Navigate("https://duckduckgo.com/");
+			form.Controls.Add(myBrowser);
+			Application.Run(form);
+		}
+	}
+}
